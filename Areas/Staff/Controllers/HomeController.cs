66ab9c500243396c5900e4424eee95f031c41f51
XR <142563759+Luvc-17@users.noreply.github.com>
2025-12-08 16:51:46 +0800

    Add Staff area files

---
+++ Areas/Staff/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PetGroomingAppointmentSystem.Areas.Staff.Controllers
+{
+    [Area("Staff")]
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            ViewData["ActivePage"] = "Appointment"; 
+            return View();
+        }
+
+        public IActionResult ApplyLeave()
+        {
+            ViewData["ActivePage"] = "ApplyLeave";
+            return View();
+        }
+
+    }
+}
