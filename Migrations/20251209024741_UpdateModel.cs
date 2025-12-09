9989e97c9373650848898ad2debb5ab4cec215bd
XR <142563759+Luvc-17@users.noreply.github.com>
2025-12-09 10:51:19 +0800

    update DB

---
+++ Migrations/20251209024741_UpdateModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PetGroomingAppointmentSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Notes",
+                table: "Admins");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Notes",
+                table: "Admins",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: true);
+        }
+    }
+}
