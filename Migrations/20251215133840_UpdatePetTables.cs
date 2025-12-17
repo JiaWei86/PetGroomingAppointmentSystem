5b09e0a21b352f33e4e6158e2774a99b7e7dd52e
JiaWei86 <jweilim806@gmail.com>
2025-12-17 17:14:48 +0800

    latest version

--- Migrations/20251215133840_UpdatePetTables.cs
+++ Migrations/20251215133840_UpdatePetTables.cs
@@ -10,22 +10,13 @@ namespace PetGroomingAppointmentSystem.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropForeignKey(
-                name: "FK_ResetPassword_Customers_CustomerId",
-                table: "ResetPassword");
-
-            migrationBuilder.DropPrimaryKey(
-                name: "PK_ResetPassword",
-                table: "ResetPassword");
-
-            migrationBuilder.RenameTable(
-                name: "ResetPassword",
-                newName: "PasswordResetTokens");
-
-            migrationBuilder.RenameIndex(
-                name: "IX_ResetPassword_CustomerId",
-                table: "PasswordResetTokens",
-                newName: "IX_PasswordResetTokens_CustomerId");
+            migrationBuilder.Sql(@"
+                IF OBJECT_ID('ResetPassword', 'U') IS NOT NULL
+                BEGIN
+                    ALTER TABLE [ResetPassword] DROP CONSTRAINT [FK_ResetPassword_Customers_CustomerId];
+                    DROP TABLE [ResetPassword];
+                END
+            ");
 
             migrationBuilder.AddColumn<string>(
                 name: "PetType",
@@ -33,144 +24,14 @@ namespace PetGroomingAppointmentSystem.Migrations
                 type: "nvarchar(20)",
                 maxLength: 20,
                 nullable: true);
-
-            migrationBuilder.AlterColumn<string>(
-                name: "VerificationCode",
-                table: "PasswordResetTokens",
-                type: "nvarchar(6)",
-                maxLength: 6,
-                nullable: true,
-                oldClrType: typeof(string),
-                oldType: "nvarchar(6)",
-                oldMaxLength: 6);
-
-            migrationBuilder.AlterColumn<string>(
-                name: "Phone",
-                table: "PasswordResetTokens",
-                type: "nvarchar(20)",
-                maxLength: 20,
-                nullable: true,
-                oldClrType: typeof(string),
-                oldType: "nvarchar(20)",
-                oldMaxLength: 20);
-
-            migrationBuilder.AlterColumn<string>(
-                name: "Email",
-                table: "PasswordResetTokens",
-                type: "nvarchar(150)",
-                maxLength: 150,
-                nullable: true,
-                oldClrType: typeof(string),
-                oldType: "nvarchar(150)",
-                oldMaxLength: 150);
-
-            migrationBuilder.AlterColumn<string>(
-                name: "CustomerId",
-                table: "PasswordResetTokens",
-                type: "nvarchar(10)",
-                maxLength: 10,
-                nullable: true,
-                oldClrType: typeof(string),
-                oldType: "nvarchar(10)",
-                oldMaxLength: 10);
-
-            migrationBuilder.AddPrimaryKey(
-                name: "PK_PasswordResetTokens",
-                table: "PasswordResetTokens",
-                column: "Id");
-
-            migrationBuilder.AddForeignKey(
-                name: "FK_PasswordResetTokens_Customers_CustomerId",
-                table: "PasswordResetTokens",
-                column: "CustomerId",
-                principalTable: "Customers",
-                principalColumn: "UserId");
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropForeignKey(
-                name: "FK_PasswordResetTokens_Customers_CustomerId",
-                table: "PasswordResetTokens");
-
-            migrationBuilder.DropPrimaryKey(
-                name: "PK_PasswordResetTokens",
-                table: "PasswordResetTokens");
-
             migrationBuilder.DropColumn(
                 name: "PetType",
                 table: "ServiceCategories");
-
-            migrationBuilder.RenameTable(
-                name: "PasswordResetTokens",
-                newName: "ResetPassword");
-
-            migrationBuilder.RenameIndex(
-                name: "IX_PasswordResetTokens_CustomerId",
-                table: "ResetPassword",
-                newName: "IX_ResetPassword_CustomerId");
-
-            migrationBuilder.AlterColumn<string>(
-                name: "VerificationCode",
-                table: "ResetPassword",
-                type: "nvarchar(6)",
-                maxLength: 6,
-                nullable: false,
-                defaultValue: "",
-                oldClrType: typeof(string),
-                oldType: "nvarchar(6)",
-                oldMaxLength: 6,
-                oldNullable: true);
-
-            migrationBuilder.AlterColumn<string>(
-                name: "Phone",
-                table: "ResetPassword",
-                type: "nvarchar(20)",
-                maxLength: 20,
-                nullable: false,
-                defaultValue: "",
-                oldClrType: typeof(string),
-                oldType: "nvarchar(20)",
-                oldMaxLength: 20,
-                oldNullable: true);
-
-            migrationBuilder.AlterColumn<string>(
-                name: "Email",
-                table: "ResetPassword",
-                type: "nvarchar(150)",
-                maxLength: 150,
-                nullable: false,
-                defaultValue: "",
-                oldClrType: typeof(string),
-                oldType: "nvarchar(150)",
-                oldMaxLength: 150,
-                oldNullable: true);
-
-            migrationBuilder.AlterColumn<string>(
-                name: "CustomerId",
-                table: "ResetPassword",
-                type: "nvarchar(10)",
-                maxLength: 10,
-                nullable: false,
-                defaultValue: "",
-                oldClrType: typeof(string),
-                oldType: "nvarchar(10)",
-                oldMaxLength: 10,
-                oldNullable: true);
-
-            migrationBuilder.AddPrimaryKey(
-                name: "PK_ResetPassword",
-                table: "ResetPassword",
-                column: "Id");
-
-            migrationBuilder.AddForeignKey(
-                name: "FK_ResetPassword_Customers_CustomerId",
-                table: "ResetPassword",
-                column: "CustomerId",
-                principalTable: "Customers",
-                principalColumn: "UserId",
-                onDelete: ReferentialAction.Cascade);
         }
     }
 }
---
+++ Migrations/20251217083733_Updated.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PetGroomingAppointmentSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class Updated : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // The PetType column was already added in 20251215133840_UpdatePetTables
+            // and the Name column was already dropped in 20251215162045_PetTable
+            // No action needed here
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // No action needed here
+        }
+    }
+}
